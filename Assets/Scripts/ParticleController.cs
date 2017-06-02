1bf34005d4128e0a36cddaedde5e58ff8c21dbec
Juan Rodriguez <timrodz@icloud.com>
2017-06-02 15:46:11 +1200

    Find all particles instead of manually asigning them

--- Assets/Scripts/ParticleController.cs
+++ Assets/Scripts/ParticleController.cs
@@ -1,25 +1,50 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ParticleController : MonoBehaviour {
-	
-	[SerializeField]
-	private List<ParticleSystem> particleList = new List<ParticleSystem>();
 
-	public void Play() {
-		
-		foreach (ParticleSystem p in particleList) {
-			p.Play();
-		}
-		
-	}
-	
-	public void Stop() {
-		
-		foreach (ParticleSystem p in particleList) {
-			p.Stop();
-		}
-		
-	}
-	
+    public Transform particleContainer;
+
+    private List<ParticleSystem> particleList = new List<ParticleSystem> ();
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start () {
+
+        if (!particleContainer) {
+            Destroy (gameObject);
+        }
+
+        for (int i = 0; i < particleContainer.childCount; i++) {
+
+            ParticleSystem p = particleContainer.GetChild (i).GetComponent<ParticleSystem> ();
+			
+            if (p != null) {
+
+                particleList.Add (p);
+
+            }
+
+        }
+
+    }
+
+    public void Play () {
+
+        foreach (ParticleSystem p in particleList) {
+            p.Play ();
+        }
+
+    }
+
+    public void Stop () {
+
+        foreach (ParticleSystem p in particleList) {
+            p.Stop ();
+        }
+
+    }
+
 }
